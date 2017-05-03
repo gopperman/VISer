2fc3c6877a216bf7efbe7e755370a86a0b3d67d3
Greg Opperman <gopperman@gmail.com>
2017-05-03 11:08:59 -0400

    Header and footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
-//import { timeScale } from './util/scales';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+        <Header />
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          LETS DO THIS
         </p>
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+  render() {
+    return (
+        <footer className="App-footer">
+          <p>Made by Greg Opperman / Amanda Erickson</p>
+        </footer>
+    )
+  }
+}
+
+export default Footer
