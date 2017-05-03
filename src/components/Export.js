aed04dfe994883f260592bac7ba8b06add477c74
Greg Opperman <gopperman@gmail.com>
2017-05-03 17:04:12 -0400

    Copy to clipboard

--- src/components/Export.js
+++ src/components/Export.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react'
+import copy from 'copy-to-clipboard';
 
 class Export extends Component {
-  render() {
-    return (
-		<div className="export">
-			<h2>OK, let's export this baby</h2>
-			<p>Copy this code into a blank file and name it <i>(whatever).svg</i></p>
-			<textarea id="export__textarea"></textarea>
-		</div>
-    )
-  }
+	copyToClipboard() {
+		copy(document.getElementById('export__textarea').value);
+	}
+	render() {
+		return (
+			<div className="export">
+				<h2>OK, let's export this baby</h2>
+				<p>Copy this code into a blank file and name it <i>(whatever).svg</i></p>
+				<textarea id="export__textarea"></textarea>
+				<p>
+					<button className="export__copy" onClick={this.copyToClipboard}>
+						Copy to clipboard
+					</button>
+				</p>
+			</div>
+		)
+	}
 }
 
 export default Export
