65443d9b17545ab08e8bb92daffedae313cab920
anhgal <americkson.me@gmail.com>
2017-05-03 17:07:40 -0400

    style playing

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,11 @@ import logo from '../logo.svg';
 class Header extends Component {
   render() {
     return (
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Viser</h2>
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+          <h1>Viser</h1>
+          <p className="intro-desc">Create instant charts with your data</p>
+
         </header>
     )
   }
