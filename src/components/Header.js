259b84f007ef37dfc8c82c9c52707213d98479a5
anhgal <americkson.me@gmail.com>
2017-05-04 10:12:21 -0400

    actually making this look good

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,7 @@ class Header extends Component {
   render() {
     return (
         <header className="app-header">
-          <img src={logo} className="app-logo" alt="logo" />
           <h1>Viser</h1>
-          <p className="intro-desc">Create instant charts with your data</p>
-
         </header>
     )
   }
