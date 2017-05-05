974bb9027e2f51775de0beae0f047de5e1e5b5eb
Greg Opperman <gopperman@gmail.com>
2017-05-05 11:37:01 -0400

    Footer tweaks

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,10 @@ class Footer extends Component {
   render() {
     return (
         <footer className="app-footer">
-		<p>Made by Greg Opperman &amp; Amanda Erickson</p>
-		<div className="taco__container">
-			<img src={taco} className="taco" alt="A taco" />
-		</div>
-
+			<div className="taco__container">
+				<img src={taco} className="taco" alt="A taco" />
+			</div>
+			<p>Made by Greg Opperman &amp; Amanda Erickson</p>
         </footer>
     )
   }
