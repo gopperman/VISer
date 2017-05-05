e19a7d1ab421a406064b32f3b0ba8bb3ec248851
Greg Opperman <gopperman@gmail.com>
2017-05-05 11:22:39 -0400

    Taco time

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import taco from '../svg/taco.svg'
 
 class Footer extends Component {
   render() {
     return (
         <footer className="app-footer">
-          <p>Made by Greg Opperman &amp; Amanda Erickson</p>
+		<p>Made by Greg Opperman &amp; Amanda Erickson</p>
+		<div className="taco__container">
+			<img src={taco} className="taco" alt="A taco" />
+		</div>
+
         </footer>
     )
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import logo from '../logo.svg'
+import logo from '../svg/logo.svg'
 
 class Header extends Component {
   render() {
